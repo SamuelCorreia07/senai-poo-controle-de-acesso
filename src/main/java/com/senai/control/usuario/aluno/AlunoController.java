983f38eb3d960f8c47b4203d6c5eb1754ab29984
Nicolas Luis Moura de Almeida <nicolasluis215@gmail.com>
2025-05-22 08:55:03 -0300

    Commit - 22/05/2025

--- src/main/java/com/senai/control/usuario/aluno/AlunoController.java
+++ src/main/java/com/senai/control/usuario/aluno/AlunoController.java
@@ -1,4 +1,42 @@
 package com.senai.control.usuario.aluno;
+import com.senai.model.usuario.aluno.Aluno;
+import com.senai.model.usuario.aluno.dao.AlunoDAO;
+import java.util.List;
 
 public class AlunoController {
+    private final AlunoDAO alunoDAO;
+
+    public AlunoController() {
+        this.alunoDAO = new AlunoDAO();
+    }
+
+    public String cadastrarAluno(String nome, int idade) {
+        Aluno novoAluno = new Aluno(0, nome, idade);
+        alunoDAO.inserir(novoAluno);
+        return "\nAlun@ cadastrad@ com sucesso!";
+    }
+
+    public String atualizarAluno(int id, String nome, int idade) {
+        Aluno aluno = alunoDAO.buscarPorId(id).orElse(null);
+        if (aluno == null) {
+            return "\nAlun@ não encontrad@!";
+        }
+        aluno.setNome(nome);
+        aluno.setIdade(idade);
+        alunoDAO.atualizar(aluno);
+        return "\nAlun@ atualizad@ com sucesso!";
+    }
+
+    public String removerAluno(int id) {
+        Aluno aluno = alunoDAO.buscarPorId(id).orElse(null);
+        if (aluno == null) {
+            return "\nAlun@ não encontrad@!";
+        }
+        alunoDAO.remover(id);
+        return "\nAlun@ removid@ com sucesso!";
+    }
+
+    public List<Aluno> listarAlunos() {
+        return alunoDAO.listarTodos();
+    }
 }
