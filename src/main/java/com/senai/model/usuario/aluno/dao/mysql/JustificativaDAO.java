a84928d770078a15ec7d199f5a904a9a3bbeb4ce
Pedro <pedrohenrickygamer@gmail.com>
2025-06-04 09:27:12 -0300

    Commit 04/06/2025

--- src/main/java/com/senai/model/usuario/aluno/dao/mysql/JustificativaDAO.java
+++ src/main/java/com/senai/model/usuario/aluno/dao/mysql/JustificativaDAO.java
@@ -1,4 +1,79 @@
 package com.senai.model.usuario.aluno.dao.mysql;
 
+import com.senai.model.conexaoMySQL.ConexaoMySQL;
+import com.senai.model.usuario.AQV;
+import com.senai.model.usuario.aluno.Justificativa;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class JustificativaDAO {
+    public void inserir(Justificativa justificativa) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO justificativa (id,nome) VALUES (?, ?)");
+            stmt.setString(1, justificativa.getId());
+            stmt.setString(2, justificativa.getNome());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void atualizar(Justificativa justificativa) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE justificativa SET id = ?,nome = ? WHERE id = ?");
+            stmt.setString(1, justificativa.getId());
+            stmt.setString(2, justificativa.getNome());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remover(int id) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM justificativa WHERE id = ?");
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Optional<Justificativa> buscarPorId(int id) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM justificativa WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return Optional.of(new AQV(
+                        rs.getInt("id"),
+                        rs.getString("nome")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+    public List<Justificativa> listarTodos() {
+        List<Justificativa> lista = new ArrayList<>();
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM justificativa");
+            while (rs.next()) {
+                lista.add(new Justificativa(
+                        rs.getInt("id"),
+                        rs.getString("nome")
+
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+}
+
 }
--- src/main/java/com/senai/model/usuario/dao/mysql/AQVDAO.java
+++ src/main/java/com/senai/model/usuario/dao/mysql/AQVDAO.java
@@ -1,5 +1,80 @@
 package com.senai.model.usuario.dao.mysql;
 
-public class AQVDAO {
+import com.senai.model.conexaoMySQL.ConexaoMySQL;
+import com.senai.model.usuario.AQV;
 
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class AQVDAO {
+    public void inserir(AQV aqv) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO aqv (id,nome) VALUES (?, ?)");
+            stmt.setString(1, aqv.getId());
+            stmt.setString(2, aqv.getNome());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void atualizar(AQV aqv) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE aqv SET id = ?,nome = ? WHERE id = ?");
+            stmt.setString(1, aqv.getId());
+            stmt.setString(2, aqv.getNome());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void remover(int id) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM aqv WHERE id = ?");
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public Optional<AQV> buscarPorId(int id) {
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM aqv WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                return Optional.of(new AQV(
+                        rs.getInt("id"),
+                        rs.getString("nome")
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+    public List<AQV> listarTodos() {
+        List<AQV> lista = new ArrayList<>();
+        try (Connection conn = ConexaoMySQL.conectar()) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM aqv");
+            while (rs.next()) {
+                lista.add(new AQV(
+                        rs.getInt("id"),
+                        rs.getString("nome")
+
+                ));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
 }
+
+
+
